dafcb3b80ee80b8b6e8945e52e688527d448d44c
Don Marks <don@zapdot.com>
2020-07-11 22:45:33 -0700

    Added new Burning Feature/FX Added Burning FX to TemplateTilemap Scene.

--- Assets/Scripts/Controllers/CTRL_Burnable.cs
+++ Assets/Scripts/Controllers/CTRL_Burnable.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.LWRP;
 using UnityEngine;
 
 public class CTRL_Burnable : MonoBehaviour
 {
     [Header("GameObjects/Components")]
     public SpriteRenderer spriteRenderer;
+    private Material spriteMaterial;
     public BoxCollider2D boxCollider;
+
+    [Header("ParticleSystem Variables")]
+    public float maxEmissionValue;
+    public ParticleSystem fireParticles;
+    public AnimationCurve fireParticleEmissCurve;
+
+    [Header("Lighting 2D Variables")]
+    public Light2D fireLight;
+    public float maxIntensity;
+
+    [Header("Collider Removal")]
+    public float colliderRemoveThreshold = 0.8f;
+
+    [Header("Shader Variables")]
+    public AnimationCurve shaderBurnCurve;
 
     [Header("Burn Variables")]
     public float timeToBurn;
@@ -23,6 +40,7 @@ public class CTRL_Burnable : MonoBehaviour
     {
         spriteRenderer = this.GetComponent<SpriteRenderer>();
         boxCollider = this.GetComponent<BoxCollider2D>();
+        spriteMaterial = spriteRenderer.material;
         startColor = spriteRenderer.color;
     }
 
@@ -34,7 +52,18 @@ public class CTRL_Burnable : MonoBehaviour
             burnTime += Time.deltaTime;
             curveEval = burnTime / timeToBurn;
             Debug.Log("curveEval = " + curveEval);
-            spriteRenderer.color = Color.Lerp(startColor,burnColor, burnCurve.Evaluate(curveEval));
+            spriteMaterial.SetFloat("_Dissolve", shaderBurnCurve.Evaluate(burnCurve.Evaluate(curveEval)));
+            var emission = fireParticles.emission;
+
+            emission.rateOverTime = fireParticleEmissCurve.Evaluate(burnCurve.Evaluate(curveEval)) * maxEmissionValue;
+            fireLight.intensity = fireParticleEmissCurve.Evaluate(burnCurve.Evaluate(curveEval)) * maxIntensity;
+
+            if (curveEval >= colliderRemoveThreshold)
+            {
+                boxCollider.enabled = false;
+            }
+
+            //spriteRenderer.color = Color.Lerp(startColor,burnColor, burnCurve.Evaluate(curveEval));
 
             if (burnTime >= timeToBurn)
             {
