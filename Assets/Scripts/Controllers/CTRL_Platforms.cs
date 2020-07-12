c04602d5d3fae115e795133515d2d7f9dc7665c4
poolen108 <poolen108@gmail.com>
2020-07-11 17:29:30 -0700

    Moving Platforms

---
+++ Assets/Scripts/Controllers/CTRL_Platforms.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTRL_Platforms : MonoBehaviour
+{
+    [Header("GameObjects/Components")]
+    public MNGR_LevelManager levelManager;
+
+    [Header("Platform Variables")]
+    private Vector3 startPos;
+    public Transform target;
+    public float speed;
+    private bool moveRight;
+
+    [Header("Art/Prototyping")]
+    public Color inactiveColor;
+    public Color activeColor;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        moveRight = true;
+    }
+
+    private void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (transform.position == target.position)
+        {
+            moveRight = false;
+        }
+        else if (transform.position == startPos)
+        {
+            moveRight = true;
+        }
+        if (moveRight == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos, step);
+        }
+        else if (moveRight)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+}
